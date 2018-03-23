9fbac11348c41852612f77f5b83c50f852bec3e7
Jonnathan Lacerda Santos <jonnathan.lacerda.santos@gmail.com>
2018-03-23 20:10:13 -0300

    Finalizado a janela modal contendo imagem e conteudo relativo aos próximos certificados a serem obtidos

---
+++ js.js
@@ -0,0 +1,63 @@
+function InnerHTMLPC(conteudo){
+	document.getElementById('conteudo-PC').innerHTML = conteudo;
+}
+function InserirConteudoPC(valor){
+	switch(valor){
+		case 1:
+		InnerHTMLPC('<img src="img/CursoDevWEB.png" class="img-responsive img-PC"><strong><u>Conteúdo/Descrição do Curso</u></strong>:<br><br>Criar páginas estáticas com HTML5, CSS3 e JavaScript; Criar páginas responsivas com Bootstrap: crie sites responsivos e que se adaptem aos diversos tamanhos de telas, como por exemplo smartphones; Utilizar a biblioteca JQuery na prática: torne a experiência de navegação no  site muito mais amigável e profissional; Aprender AJAX: um conceito de requisição assíncrona que torna a usabilidade de sites e sistemas web muito mais agradáveis; Aprender PHP: uma das linguagens de programação mais utilizadas no mundo; Aprender o conceito de Orientação a Objetos; Aprender MySQL: um dos bancos de dados mais utilizados no mundo.<br><br><div class="progress"><div style="width: 67%"><div class="progress-bar"></div></div></div><strong>67% Concluído</strong><a href="https://www.udemy.com/curso-completo-do-desenvolvedor-web/learn/v4/overview">Fonte: Udemy - Visitar página do Curso</a>');
+		break;
+		case 2: 
+		InnerHTMLPC('<img src="img/CursoDevBD.png" class="img-responsive img-PC"><strong><u>Conteúdo/Descrição do Curso</u></strong>:<br><br>Entender a diferença entre um Administrador de Dados e um DBA; Criar bancos de dados consistentes do ponto de infraestrutura e modelagem; Instalar o Banco de Dados MySql; Executar a linguagem SQL - Structured Query Language, ou Linguagem de Consulta Estruturada em QUALQUER banco de dados; Entender todo o ambiente trasacional e optar por continuar seus estudos em ambientes analíticos de Business Intelligence; Instalar o Banco de Dados Oracle; Instalar o Banco de Dados SQL Server; Modelar a base de dados para qualquer sistema transacional; Programar em Banco de Dados; Realizar Backups e Restores dos seus Bancos de Dados; Aplicar Constraints de qualquer natureza em suas tabelas; Aplicar as Formas Normais; Criar Triggers, Procedures, Functions e Views; Escolher as funções nativas de qualquer banco de dados, de acordo com a sua necessidade; Utilizar o Dicionário de Dados; Estar seguro na disciplina Banco de Dados; Realizar downloads de softwares relacionados a banco de dados; Utilizar softwares de modelagem; Aplicar seguramente os relacionamentos 1 x 1 , 1 x N, N x N, Generalização e Autorelacionamento, entendendo o porquê de cada uma. <br><br><div class="progress"><div style="width: 0%"><div class="progress-bar"></div></div></div><strong>0% Concluído</strong><a href="https://www.udemy.com/bancos-de-dados-relacionais-basico-avancado/learn/v4/overview">Fonte: Udemy - Visitar página do Curso</a>');
+		break;
+		case 3: 
+		InnerHTMLPC('<img src="img/CursoDevPYTHON.png" class="img-responsive img-PC"><strong><u>Conteúdo/Descrição do Curso</u></strong>:<br><br>Ambientes Virtuais (Virtualenv e Anaconda); Controle de Versionamento de Código (Git); 	Construção de janelas gráficas (Kivy); Banco de dados relacional (SQLite e MySQL); Linguagem SQL; Framework ORM (SQLAlchemy); Manipulação de linguagens de marcação (XML/JSON/HTML); Download de arquivos e/ou vídeos; Consumo de serviços de terceiros; Integração com serviços de mensagem (envio de torpedo e WhatsApp); Integração com a Nuvem (Firebase); Sincronização de dados em tempo real entre vários dispositivos<br><br><div class="progress"><div style="width: 4%"><div class="progress-bar"></div></div></div><strong>04% Concluído</strong><a href="https://www.udemy.com/curso-completo-do-desenvolvedor-web/learn/v4/">Fonte: Udemy - Visitar página do Curso</a>');
+		break;
+		case 4: 
+		InnerHTMLPC('<img src="img/CursoIngles.png" class="img-responsive img-PC"><strong><u>Conteúdo/Descrição do Curso</u></strong>:<br><br>Ler; Escrever; Ouvir; Falar; It; The; In/on/at; Need; Casa; Trabalho; Dinheiro; Bancos; Substantivos; Perguntas; Comparações; As Cores; Progressive aspects; Supermercado; Roupas; Cozinhar; Falar "Não!"; Parques; O Alfabeto; Computadores; Futuro; Presente; Passado; Palavras de WH-Posse; Todas as principais conjugações verbais; E muito, muito mais; <br><br><div class="progress"><div style="width: 9%"><div class="progress-bar"></div></div></div><strong>09% Concluído</strong><a href="https://www.udemy.com/o-curso-completo-de-ingles/learn/v4/overview">Fonte: Udemy - Visitar página do Curso</a>');
+		break;
+		case 5: 
+		InnerHTMLPC('<img src="img/CursoDevAndroid.png" class="img-responsive img-PC"><strong><u>Conteúdo/Descrição do Curso</u></strong>:<br><br>Aprenda fundamentos da programação Java, a linguagem utilizada para desenvolver Android; Saiba como projetar interfaces com o Android Studio; Aprenda os elementos de interface e a estrutura de projetos Android; Entenda as arrays, loops, orientação a objetos e estruturas condicionais; Construa 15 aplicativos durante o curso; Aprenda a publicar os seus apps; <br><br><div class="progress"><div style="width: 0%"><div class="progress-bar"></div></div></div><strong>0% Concluído</strong><a href="https://www.udemy.com/curso-completo-do-desenvolvedor-android/learn/v4/overview">Fonte: Udemy - Visitar página do Curso</a>');
+		break;
+		case 6: 
+		InnerHTMLPC('<img src="img/CursoDevMultiplataforma.png" class="img-responsive img-PC"><strong><u>Conteúdo/Descrição do Curso</u></strong>:<br><br>Aprenderá a como preparar o ambiente de desenvolvimento tanto no Windows quanto no Mac OS; Irá adquirir conhecimentos básicos e intermediários da linguagem JavaScript; Aprenderá a utilizar a biblioteca React; Aprenderá a utilizar o framework React Native; Aprenderá a utilizar o framework Redux; Aprenderá a utilizar o banco de dados Firebase; Aprenderá a publicar os seus apps na Google Play e Apple Store; <br><br><div class="progress"><div style="width: 0%"><div class="progress-bar"></div></div></div><strong>0% Concluído</strong><a href="https://www.udemy.com/desenvolvedor-multiplataforma-androidios-com-react-e-redux/learn/v4/overview">Fonte: Udemy - Visitar página do Curso</a>');
+		break;
+		case 7: 
+		InnerHTMLPC('<img src="img/CursoDevUnreal.png" class="img-responsive img-PC"><strong><u>Conteúdo/Descrição do Curso</u></strong>:<br><br>Programação; Level Design; Inteligência Artificial; Game Design; Prototipação; Planejamento; UML; E muito mais... <br><br><div class="progress"><div style="width: 0%"><div class="progress-bar"></div></div></div><strong>0% Concluído</strong><a href="https://www.udemy.com/como-criar-seu-game/learn/v4/overview">Fonte: Udemy - Visitar página do Curso</a>');
+		break;
+		case 8: 
+		InnerHTMLPC('<img src="img/CursoDevUnity.png" class="img-responsive img-PC"><strong><u>Conteúdo/Descrição do Curso</u></strong>:<br><br>Manipular a Game Engine; Programar jogos usando a linguagem C#; Usar todos os componentes físicos 2D; Preparar Sprites animadas; Criar animações 2D dentro da Unity; Mesclar arte 2D com 3D; Usar Realidade Aumentada; Monetização de jogos; Publicação no Google Play; Divulgação do jogo; <br><br><div class="progress"><div style="width: 0%"><div class="progress-bar"></div></div></div><strong>0% Concluído</strong><a href="https://www.udemy.com/desenvolvimento-de-jogos-2d-para-android-com-unity-5/learn/v4/overview">Fonte: Udemy - Visitar página do Curso</a>');
+		break;
+		case 9: InnerHTMLPC('<img src="img/CursoDevGodot.png" class="img-responsive img-PC"><strong><u>Conteúdo/Descrição do Curso</u></strong>:<br><br>Criar diversos games para Android e iOS conforme suas necessidades; Publicar seu Game na Play Store; Utilizar os diversos recursos presentes na Engine Godot; Compreender os princípios essenciais da Criação de Games; Utilizar o algoritmo A* para inteligência artificial; <br><br><div class="progress"><div style="width: 0%"><div class="progress-bar"></div></div></div><strong>0% Concluído</strong><a href="https://www.udemy.com/criacao-de-jogos-para-android-curso-completo/learn/v4/overview">Fonte: Udemy - Visitar página do Curso</a>');
+		break;		
+		default: break;
+	}
+}
+function Mostrar(id){
+	document.getElementById(id).style.display = 'block';
+}
+function Ocultar(id){
+	document.getElementById(id).style.display = 'none';
+}
+function InnerHTML(conteudo) {
+	document.getElementById('conteudo').innerHTML = conteudo
+}
+function Inserir(id){
+	switch(id){
+		case 1:
+		InnerHTML("");
+		var elemento = document.createElement("img");
+		elemento.src = 'img/foto3x4.jpg';
+		elemento.className = 'img-responsive img-rounded foto';
+		document.getElementById('conteudo').appendChild(elemento);
+		break;
+		case 2:
+		InnerHTML("<br><span class='glyphicon glyphicon-info-sign'></span> 26 Anos<br class='hidden-xs'> <br class='hidden-xs'><span class='glyphicon glyphicon-heart'></span> Solteiro<br class='hidden-xs'> <br class='hidden-xs'><span class='glyphicon glyphicon-globe'></span> Brasileiro");
+		break;
+		case 3:
+		InnerHTML("<br>Rua Antônio Pires, nº 240 - Vila Albertina<br>CEP: 02730-000 <br class='hidden-xs'>São Paulo - SP");
+		break;
+		case 4:
+		InnerHTML("(11) 94184-4914<br class='hidden-xs'> Ou 95241-9622<br><a href='mailto:jonnathan.santos@fatec.sp.gov.br'>jonnathan.santos<br class='hidden-xs'>@fatec.sp.gov.br</a><br><a href='mailto:jonnathan.lacerda.santos@gmail.com'>jonnathan.lacerda<br class='hidden-xs'>.santos@gmail.com</a>");
+		break;
+	}
+}
